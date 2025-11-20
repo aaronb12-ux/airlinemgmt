b1459ea1c278ad56b76b45d57f99163ed7c3cd6a
aaronb12-ux <afairster03@gmail.com>
2025-11-20 12:54:40 -0800

    added csv reading logic for flights

--- main.cpp
+++ main.cpp
@@ -1,29 +1,94 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <format>
 #include "Passenger.hpp"
 #include "Flight.hpp"
 
-// keep function as-is; tests will link against this TU by including a forward declaration
-void displayWelcome() {
+void displayWelcome()
+{
+
     std::cout << "|--------------------------|" << std::endl;
     std::cout << "|Airline Reservation System|" << std::endl;
     std::cout << "|--------------------------|" << std::endl;
     std::cout << "|1. Book Flight            |" << std::endl;
-    std::cout << "|2. View Reservations      |" << std::endl; 
+    std::cout << "|2. View Reservations      |" << std::endl;
     std::cout << "|3. Cancel Booking         |" << std::endl;
     std::cout << "|4. Exit                   |" << std::endl;
     std::cout << "|--------------------------|" << std::endl;
 }
 
-int main() {
-
-    displayWelcome();
-    int choice;
+char makeChoice()
+{
+    char choice;
     std::cout << "Make your selection: ";
     std::cin >> choice;
+    return choice;
+}
 
-    Passenger p1("aaron", "bernstein");
+std::vector<Flight> getFlights()
+{
+    std::vector<Flight> flights;
 
-    std::cout << p1.firstName;
+    std::ifstream file("flights.csv");
+
+    if (!file.is_open())
+    { // failed to open file
+        std::cerr << "Failed to open file: flights.csv " << std::endl;
+        return flights;
+    }
+
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+
+        std::vector<std::string> row; // vector of strings
+        std::stringstream ss(line);
+        std::string cell;
+
+        while (std::getline(ss, cell, ','))
+        {
+            row.push_back(cell);
+        }
+
+        Flight newFlight(std::stoi(row[0]), row[1], row[2], row[3], row[4]); //constructing new flight
+        flights.push_back(newFlight);
+    }
+    file.close();
+
+    return flights;
+}
+
+void showFlights(std::vector<Flight> flights) {
+
+    for (const auto& flight : flights) {
+        std::cout << "\n";
+        std::cout << "Flight Number: " + std::to_string(flight.flightID) << std::endl;
+        std::cout << "Source: " + flight.source << std::endl;
+        std::cout << "Destination: " + flight.destination << std::endl;
+        std::cout << "Take Off: " + flight.takeOffTime << std::endl;
+        std::cout << "Landing: " + flight.landTime << std::endl;
+    }
+    std::cout << "\n";
+
+}
+
+int main()
+{
+
+    displayWelcome();
+    char choice = makeChoice();
+
+    if (choice == '1')
+    {
+        // load in flight info from flights.csv
+        // store in vector of Flight objects
+        auto flights = getFlights();
+        showFlights(flights);
+
+    }
 
     return 0;
 }
